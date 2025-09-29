cebceb3b436d5de6fe57b09d0f8a18432f1c79b1
gsahnn <gokhansah09@gmail.com>
2025-09-29 20:51:56 +0300

    123132

---
+++ Solara ONLİNE/Assets/_Project/Scripts/UI/Nameplate_Controller.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class Nameplate_Controller
+{
+    
+}
--- Solara ONLİNE/Assets/_Project/Scripts/UI/TargetFrame_Controller.cs
+++ Solara ONLİNE/Assets/_Project/Scripts/UI/TargetFrame_Controller.cs
@@ -6,11 +6,16 @@ public class TargetFrame_Controller : MonoBehaviour
 {
     public static TargetFrame_Controller Instance { get; private set; }
 
+    [Header("UI Referanslarý")]
     [SerializeField] private GameObject targetFramePanel;
     [SerializeField] private TextMeshProUGUI targetNameText;
     [SerializeField] private Slider targetHealthSlider;
 
+    [Header("Dünya Referanslarý")]
+    [SerializeField] private GameObject targetCirclePrefab;
+
     private CharacterStats currentTargetStats;
+    private GameObject currentTargetCircleInstance;
 
     private void Awake()
     {
@@ -19,31 +24,34 @@ public class TargetFrame_Controller : MonoBehaviour
 
     public void Initialize(PlayerTargeting playerTargeting)
     {
-        playerTargeting.OnTargetSelected += UpdateTargetFrame;
-        targetFramePanel.SetActive(false);
+        if (playerTargeting != null)
+            playerTargeting.OnTargetSelected += UpdateTargetFrame;
+
+        if (targetFramePanel != null)
+            targetFramePanel.SetActive(false);
     }
 
     private void OnDestroy()
     {
-        // Oyuncu yok edildiðinde event aboneliðini sonlandýrmak önemlidir.
         PlayerTargeting playerTargeting = FindFirstObjectByType<PlayerTargeting>();
-        if (playerTargeting != null)
-        {
-            playerTargeting.OnTargetSelected -= UpdateTargetFrame;
-        }
+        if (playerTargeting != null) playerTargeting.OnTargetSelected -= UpdateTargetFrame;
     }
 
     private void UpdateTargetFrame(Transform newTarget)
     {
-        // Önceki hedefin event'inden aboneliði iptal et.
-        if (currentTargetStats != null)
-            currentTargetStats.OnStatsChanged -= UpdateHealthBar;
+        if (currentTargetCircleInstance != null) Destroy(currentTargetCircleInstance);
+        if (currentTargetStats != null) currentTargetStats.OnStatsChanged -= UpdateHealthBar;
 
         if (newTarget != null && newTarget.TryGetComponent(out currentTargetStats))
         {
             targetFramePanel.SetActive(true);
             currentTargetStats.OnStatsChanged += UpdateHealthBar;
-            UpdateHealthBar(); // Ýlk deðerleri ata.
+            UpdateHealthBar();
+
+            if (targetCirclePrefab != null)
+            {
+                currentTargetCircleInstance = Instantiate(targetCirclePrefab, newTarget.position, newTarget.rotation, newTarget);
+            }
         }
         else
         {
@@ -56,7 +64,7 @@ public class TargetFrame_Controller : MonoBehaviour
     {
         if (currentTargetStats != null)
         {
-            targetNameText.text = currentTargetStats.gameObject.name;
+            targetNameText.text = $"Lv. {currentTargetStats.level} {currentTargetStats.gameObject.name}";
             targetHealthSlider.maxValue = currentTargetStats.maxHealth;
             targetHealthSlider.value = currentTargetStats.currentHealth;
         }
